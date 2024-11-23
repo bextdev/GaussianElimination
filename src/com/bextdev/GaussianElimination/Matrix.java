51be3e92d61ad8566584d773438c74b0fbfb401a
bextdev <angn9096@gmail.com>
2024-11-23 21:00:42 +0800

    Create Matrix.java

---
+++ src/com/bextdev/GaussianElimination/Matrix.java
@@ -0,0 +1,35 @@
+public class Matrix {
+    private double[][] augmentedMatrix;
+    private int n;
+
+    public Matrix(double[][] A, double[] b) {
+        this.n = A.length;
+        this.augmentedMatrix = new double[n][n + 1];
+
+        // Form the augmented matrix [A|b]
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                augmentedMatrix[i][j] = A[i][j];
+            }
+            augmentedMatrix[i][n] = b[i];
+        }
+    }
+
+    public double[][] getAugmentedMatrix() {
+        return augmentedMatrix;
+    }
+
+    public int getSize() {
+        return n;
+    }
+
+    // Utility method to print matrix (for debugging)
+    public void printMatrix() {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n + 1; j++) {
+                return augmentedMatrix[i][j] + " ";
+            }
+                return;
+        }
+    }
+}
